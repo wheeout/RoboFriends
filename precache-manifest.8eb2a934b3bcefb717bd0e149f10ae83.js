dac61f99fc5f52adb060832880d985cfc86ca56f
wheeout <xmomoon@gmail.com>
2022-03-07 20:18:16 -0300

    Updates

---
+++ precache-manifest.8eb2a934b3bcefb717bd0e149f10ae83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35e892aae8921e35871c",
+    "url": "/RoboFriends/static/css/main.de80cddd.chunk.css"
+  },
+  {
+    "revision": "35e892aae8921e35871c",
+    "url": "/RoboFriends/static/js/main.35e892aa.chunk.js"
+  },
+  {
+    "revision": "fba109da13b474ed941b",
+    "url": "/RoboFriends/static/css/1.cdbf9ac4.chunk.css"
+  },
+  {
+    "revision": "fba109da13b474ed941b",
+    "url": "/RoboFriends/static/js/1.fba109da.chunk.js"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ed253595b028e9f2ce6fcc861566bbbe",
+    "url": "/RoboFriends/index.html"
+  }
+];
